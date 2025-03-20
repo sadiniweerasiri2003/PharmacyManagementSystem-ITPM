f262a511c137860591e0b07a3b74d38d6701cef0
Sadini Weerasiri <sadini20030104@gmail.com>
2025-03-20 15:45:45 +0530

    backend crud by sadini
    create dthe basic crud for backend

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 
 const authRoutes = require("./routes/authRoutes");
+const supplierRoutes = require("./routes/supplierRoutes");
 
 const app = express();
 app.use(express.json());
@@ -12,6 +13,7 @@ app.use(cors());
 connectDB();
 
 app.use("/api/auth", authRoutes);
+app.use("/api/suppliers", supplierRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
